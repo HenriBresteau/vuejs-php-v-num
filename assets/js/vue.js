4f983a571dcb0f1850a7f152668e8775be68bbb6
henriB <henri.bretseau@gmail.com>
2020-11-16 21:00:15 +0100

    countryList +countryOption

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,34 +1,53 @@
 const vue = new Vue({
-    data: () => {
-        return {
-            wines:[],
-            searchKey:"",
-            inputType:""
-        }
+  data: () => {
+    return {
+      wines: [],
+      searchKey: "",
+      inputType: "",
+      countryList: [],
+      contryOption: [],
+    };
+  },
+  computed: {
+    search() {
+      return this.wines.filter((wine) => {
+        return wine.name.toLowerCase().includes(this.searchKey.toLowerCase());
+      });
     },
-    computed: {
-        search(){
-            return this.wines.filter((wine) => {
-                return wine.name.toLowerCase().includes(this.searchKey.toLowerCase());
-            })
-        }
+  },
+  methods: {
+    removeItem(id) {
+      this.$delete(this.wines, id);
     },
-    methods: {
-        removeItem(id){
-            this.$delete(this.wines, id);
-        },
-        getImgUrl(pic){
-            return "assets/uploads/"+pic;
-        },
-        searchInput(param){
-            this.inputType = param;
-        }
+    getImgUrl(pic) {
+      return "assets/uploads/" + pic;
     },
-    mounted(){
-        axios.get("libraries/controllers/getData.php")
-        .then((res) => res.data)
-        .then((res)=> {
-            this.wines = res;
+    searchInput(param) {
+      this.inputType = param;
+    },
+  },
+  mounted() {
+    axios
+      .get("libraries/controllers/getData.php")
+      .then((res) => res.data)
+      .then((res) => {
+        this.wines = res;
+      })
+      .then(() => {
+        for (let i = 0; i < this.wines.length; i++) {
+          if (!this.countryList.includes(this.wines[i].country)) {
+            this.countryList.push(this.wines[i].country);
+          }
+        }
+      });
+    setTimeout(() => {
+      let arr = this.countryList.sort();
+      for (let index = 0; index < arr.length; index++) {
+        this.contryOption.push({
+          name: arr[index],
+          id: arr[index],
         });
-    }
+      }
+    }, 500);
+  },
 }).$mount("#vue-app");

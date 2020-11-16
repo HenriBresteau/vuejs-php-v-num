85d3af96b76b38b925edc800e14483dadb706362
henriB <henri.bretseau@gmail.com>
2020-11-16 19:31:50 +0100

    buttons+ text

--- libraries/views/lobby.html.php
+++ libraries/views/lobby.html.php
@@ -20,6 +20,20 @@
           <h2> {{wine.name}} </h2>
           <i class="fas fa-times" @click="removeItem(id)"></i>
         </div>
+        <div class="container">
+          <div class="text-container">
+            <div class="buttons">
+              <h4> {{wine.year}} </h4>
+              <h4> {{wine.country}} </h4>
+              <h4> {{wine.grapes}} </h4>
+            </div>
+            <div class="location">
+              <i class="fas fa-map-marker-alt"></i>
+              <span> {{wine.region}} </span>
+            </div>
+            <p> {{wine.description}} </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>

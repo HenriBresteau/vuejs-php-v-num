96fef705a95fab8b051794201af8b1dbb84e56b5
henriB <henri.bretseau@gmail.com>
2020-11-16 17:52:15 +0100

    config database

---
+++ libraries/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+// Retourne une connexion à la DB
+//  @return PDO
+
+function getPDO (): PDO {
+
+    $serveurname= 'localhost';
+    $dbname= 'vivino';
+    $user ='root';
+    $pass = '';
+
+    $pdo = new PDO("mysql:host=$serveurname;dbname=$dbname", $user, $pass, [
+        PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,
+        // mode de requete par défaut => renvoit des tableaux associatifs
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    return $pdo;
+}
